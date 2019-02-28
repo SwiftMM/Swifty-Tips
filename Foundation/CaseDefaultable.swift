ffafef7263e2e8a8d0fa375cf28024bfca1e4eee
Meow <gao497868860@gmail.com>
2019-02-28 11:53:24 +0800

    Create CaseDefaultable.swift

---
+++ Foundation/CaseDefaultable.swift
@@ -0,0 +1,11 @@
+public protocol CaseDefaultable: RawRepresentable {
+    static var defaultCase: Self { get }
+}
+
+public extension CaseDefaultable where Self: Decodable, Self.RawValue: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(RawValue.self)
+        self = Self.init(rawValue: rawValue) ?? Self.defaultCase
+    }
+}
